f2d0a7859fecf517bea752173f4997eef387ed44
stevencarlislewalker <scwalker@math.mcmaster.ca>
2016-07-16 20:59:49 -0400

    graphs

--- contraction.R
+++ contraction.R
@@ -15,15 +15,29 @@ dat$startTime <- as.POSIXct(dat$startTime, format = "%Y-%m-%d %H:%M:%S", tz = "U
 dat$duration  <-     seconds(dat$duration)
 
 dat <-
-  dat %>%
-  mutate(endTime = startTime + duration)
+    dat %>%
+    mutate(endTime = startTime + duration) %>%
+    mutate(durationRest = as.duration(endTime - lag(startTime))) %>%
+    mutate(contractionID = seq_along(startTime)) %>%
+    mutate(durationRatio = as.numeric(duration) / as.numeric(durationRest)) %>%
+    mutate(interval = as.duration(startTime - lag(startTime)))
+
+intervalPlot <-
+    dat %>%
+    ggplot() + theme_bw() + 
+    geom_line(aes(contractionID, as.numeric(interval) / 60)) +
+    geom_ribbon(aes(x = contractionID, ymin = 3, ymax = 4),
+                alpha = 0.4) +
+    geom_smooth(aes(contractionID, as.numeric(interval) / 60),
+                method = "lm", formula = y ~ log(x)) +
+    scale_x_continuous("Contraction ID") +
+    scale_y_continuous("Inter-contraction interval (min)")
 
 dateTicks <-
     paste0("2016-07-08 ", c("6:00", "9:00", "12:00", "15:00", "18:00"), ":00") %>%
       as.POSIXct(format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
 
-## plot raw data
-pp <-
+durationPlot <-
     dat %>%
       ggplot() + theme_bw() +
       geom_segment(aes(x = startTime, xend = startTime,
@@ -33,14 +47,6 @@ pp <-
                        breaks = dateTicks,
                        labels = date_format("%H:%M", tz = "UTC")) +
       scale_y_continuous("Contraction duration (s)")
-print(pp)
-ggplotly(pp)
 
-pp <-
-    dat %>%
-      ggplot() + theme_bw() +
-      geom_segment(aes(x = startTime, xend = endTime,
-                       y = as.numeric(duration), yend = as.numeric(duration)))
-
-dat %>%
-
+ggsave("intervalPlot.png", intervalPlot)
+ggsave("durationPlot.png", durationPlot)
