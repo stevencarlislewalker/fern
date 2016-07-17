ead22cefbdb20302dff1b223541c46c3206e5c5f
stevencarlislewalker <scwalker@math.mcmaster.ca>
2016-07-17 08:32:31 -0400

    trying different models

--- contraction.R
+++ contraction.R
@@ -9,7 +9,8 @@ dat <-
     fread("contractionData.csv",
           colClasses = c("character", "integer")) %>%
       mutate(startTime = ifelse(startTime == "", NA,
-                 paste0("2016-07-08 ", startTime, ":00")))
+                 paste0("2016-07-08 ", startTime, ":00"))) %>%
+     mutate(contractionID = seq_along(startTime))
 
 dat$startTime <- as.POSIXct(dat$startTime, format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
 dat$duration  <-     seconds(dat$duration)
@@ -18,7 +19,6 @@ dat <-
     dat %>%
     mutate(endTime = startTime + duration) %>%
     mutate(durationRest = as.duration(endTime - lag(startTime))) %>%
-    mutate(contractionID = seq_along(startTime)) %>%
     mutate(durationRatio = as.numeric(duration) / as.numeric(durationRest)) %>%
     mutate(interval = as.duration(startTime - lag(startTime)))
 
@@ -61,3 +61,25 @@ durationPlot <-
 ggsave("timePlot.png", timePlot)
 ggsave("intervalPlot.png", intervalPlot)
 ggsave("durationPlot.png", durationPlot)
+
+plotCurve <- function(alpha, k1, k2) {
+    x <- 1:nrow(dat)
+    y <- exp(k1) * exp(- alpha * x) + exp(k2) * (1 - exp(- alpha * x))
+    plot(x, y, type = "l", ylim = c(0, 55), lwd = 3, las = 1,
+         xlab = "Contraction ID", ylab = "Inter-contraction interval (min)")
+    with(dat, lines(contractionID, as.numeric(interval) / 60))
+    abline(h = c(exp(k1), exp(k2)), lty = 2)
+}
+plotCurve(0.05, 3.5, 1.2)
+
+nonLinearFormula <- 
+    as.numeric(interval) / 60 ~
+    exp(k1) * exp(- alpha * contractionID) + exp(k2) * (1 - exp(- alpha * contractionID))
+mod <- nls(nonLinearFormula, data = dat, start = list(alpha = 0.05, k1 = 3.5, k2 = 1.2))
+do.call(plotCurve, as.list(coef(mod)))
+summary(mod)
+
+## confidence interval on the estimated asymptotic inter-contraction interval
+k2Hat <- coef(mod)["k2"]
+k2SE <- sqrt(vcov(mod)["k2", "k2"])
+exp(k2Hat + c(lower = -1.96 * k2SE, estimate = 0, upper = 1.96 * k2SE))
